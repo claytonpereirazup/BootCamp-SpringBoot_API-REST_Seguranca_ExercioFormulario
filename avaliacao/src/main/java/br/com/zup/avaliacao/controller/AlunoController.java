3add26da285f23bfa9a21fc7b1abe30c03a02e24
Clayton Pereira <clayton.pereira@zup.com.br>
2021-06-28 22:01:05 -0300

    Project - Flexible Pagination Added

--- avaliacao/src/main/java/br/com/zup/avaliacao/controller/AlunoController.java
+++ avaliacao/src/main/java/br/com/zup/avaliacao/controller/AlunoController.java
@@ -7,16 +7,15 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -43,13 +42,23 @@ public class AlunoController {
 //	return Aluno.converteEntidadeParaDto(alunos);
 //	}
 	
+//	@GetMapping
+//	public Page<AlunoDto> listar(@RequestParam int pagina, @RequestParam int qtdRegistro, @RequestParam String ordenacao) {
+//	//lista em memória
+//	//Aluno aluno = new Aluno(1L, "Clayton Pereira", 48, "clayton.pereira@zup.com.br");
+//	//return aluno;
+//		
+//	Pageable paginacao = PageRequest.of(pagina, qtdRegistro, Direction.DESC ,ordenacao);
+//		
+//	Page<Aluno> alunos = alunoRepository.findAll(paginacao);
+//	return Aluno.converteEntidadeParaDto(alunos);
+//	}
+	
 	@GetMapping
-	public Page<AlunoDto> listar(@RequestParam int pagina, @RequestParam int qtdRegistro, @RequestParam String ordenacao) {
+	public Page<AlunoDto> listar(@PageableDefault(sort="id", direction=Direction.ASC,page = 0, size = 10) Pageable paginacao) {
 	//lista em memória
 	//Aluno aluno = new Aluno(1L, "Clayton Pereira", 48, "clayton.pereira@zup.com.br");
 	//return aluno;
-		
-	Pageable paginacao = PageRequest.of(pagina, qtdRegistro, Direction.DESC ,ordenacao);
 		
 	Page<Aluno> alunos = alunoRepository.findAll(paginacao);
 	return Aluno.converteEntidadeParaDto(alunos);
